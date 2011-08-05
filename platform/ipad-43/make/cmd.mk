792323e931f407eda9fcb8cfc48277ad7a591869
zenzhang <zenzhang@pptv.com>
2011-08-05 06:27:12 +0000

    ipad42 commit
    git-svn-id: http://svn01/repositories/r01/base@432 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ platform/ipad-43/make/cmd.mk
@@ -0,0 +1,5 @@
+PLATFORM_TOOL_PATH	:= /toolchain /Developer/Platforms/iPhoneOS.platform/Developer/usr/bin /Developer/usr/bin
+
+PLATFORM_TOOL_NAME	:= gcc42
+
+PLATFORM_TOOL_PREFIX	:= ipad-42-
---
+++ platform/ipad-43/make/flags.mk
@@ -0,0 +1,1 @@
+PLATFORM_COMPILE_FLAGS	:= -arch armv7 -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.3.sdk -gdwarf-2 -mthumb
