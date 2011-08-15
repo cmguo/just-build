e493fab11405da44deeab866549e3b23eb0823d3
isxxguo <isxxguo@pptv.com>
2011-08-15 10:09:45 +0000

    /base/make/root.mk 支持输入平台名带有/
    git-svn-id: http://svn01/repositories/r01/base@523 b3a1389e-2159-5c43-9038-463bf877d0d9

--- make/root.mk
+++ make/root.mk
@@ -11,9 +11,13 @@ include $(ROOT_MAKE_DIRECTORY)/cmd.mk
 
 PLATFORM_NAME_ALL	:= $(notdir $(wildcard $(ROOT_PLATFORM_DIRECTORY)/*))
 
-PLATFORM_TARGETS	:= $(filter $(PLATFORM_NAME_ALL) all-platform,$(MAKECMDGOALS))
+MAKECMDGOALS_NO_SLASH	:= $(patsubst %/,%,$(MAKECMDGOALS))
+
+PLATFORM_TARGETS	:= $(filter $(PLATFORM_NAME_ALL) all-platform,$(MAKECMDGOALS_NO_SLASH))
 
 OTHER_TARGETS		:= $(filter-out $(PLATFORM_TARGETS),$(MAKECMDGOALS))
+
+PASS_TARGETS		:= $(filter-out $(addsuffix /,$(PLATFORM_TARGETS)),$(OTHER_TARGETS))
 
 ifeq ($(PLATFORM_TARGETS),)
 	PLATFORM_TARGETS	:= $(wildcard *)
@@ -28,7 +32,7 @@ all-platform : $(PLATFORM_NAME_ALL)
 .PHONY: $(PLATFORM_NAME_ALL)
 $(PLATFORM_NAME_ALL) : % : boot//%
 	@echo $@
-	$(MAKE) -C $@ config="$(strip $(config))" $(OTHER_TARGETS)
+	$(MAKE) -C $@ config="$(strip $(config))" $(PASS_TARGETS)
 	@echo
 
 .PHONY: $(OTHER_TARGETS)
