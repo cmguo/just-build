b31901ca84b2c1d74a8e346a0ed6e3be71b8ecd1
zenzhang <zenzhang@pptv.com>
2012-03-07 08:39:28 +0000

    1.0.0/make/proj/target/static.mk 新增多个.a文件打包成一个.a功能
    git-svn-id: http://svn01/repositories/r01/base@2867 b3a1389e-2159-5c43-9038-463bf877d0d9

--- make/proj/target/static.mk
+++ make/proj/target/static.mk
@@ -4,6 +4,11 @@
 ## @brief	生成静态链接库的规则
 ## @version	1.0
 ###############################################################################
+
+ifeq ($(CONFIG_combine_static_lib),yes)
+        include $(TARGET_MAKE_DIRECTORY)/link.mk
+endif
+
 $(TARGET_FILE_FULL): $(SOURCE_OBJECTS) $(HEADER_OBJECTS) $(MAKEFILE_LIST)
 	@$(RM) $@
-	$(AR) $(ARFLAGS) $@ $(addprefix $(OBJECT_DIRECTORY)/, $(SOURCE_OBJECTS)) 
+	$(AR) $(ARFLAGS) $@ $(addprefix $(OBJECT_DIRECTORY)/, $(SOURCE_OBJECTS)) $(DEPEND_FILES)
