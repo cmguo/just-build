d9f181ab1c83c8de3861e52e80c8ef3207c57a48
isxxguo <isxxguo@pptv.com>
2012-04-20 03:18:31 +0000

    /base/make func/macro.mk 增加判断宏是否定义的函数macro_is_defined
    git-svn-id: http://svn01/repositories/r01/base@3298 b3a1389e-2159-5c43-9038-463bf877d0d9

--- make/func/macro.mk
+++ make/func/macro.mk
@@ -5,15 +5,27 @@
 ## @version	1.0
 ###############################################################################
 
-# 通用提取宏定义
+# 提取宏定义
 # argment1:     文件名
-# argment2:     提取的信息项
-# return:       该信息项的值
+# argment2:	宏名称
+# return:       宏定义的值
 
 define get_macro_info
 $(strip \
         $(shell PATH=$(PATH) LD_LIBRARY_PATH=$(LD_LIBRARY_PATH) $(CC) -E $(1) $(addprefix -I,$(wildcard $(HEADER_DIRECTORYS))) -dM | \
 		awk '$$2 == "$(2)" { gsub("\"","",$$3); print $$3; }') \
+)
+endef
+
+# 判断宏是否定义
+# argment1:     文件名
+# argment2:	宏名称
+# return:	有定义返回宏名称，否则返回空串
+
+define macro_is_defined
+$(strip \
+        $(shell PATH=$(PATH) LD_LIBRARY_PATH=$(LD_LIBRARY_PATH) $(CC) -E $(1) $(addprefix -I,$(wildcard $(HEADER_DIRECTORYS))) -dM | \
+		awk '$$2 == "$(2)" { gsub("\"","",$$3); print $(2); }') \
 )
 endef
 
