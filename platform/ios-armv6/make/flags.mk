c3b5fe2641178f77dcd3a6d8284355e67dc05437
zenzhang <zenzhang@pptv.com>
2012-05-22 02:05:57 +0000

    1.0.0/platform/ios-armv*/make/flags.mk 修正编译出现out of range错误
    git-svn-id: http://svn01/repositories/r01/base@3638 b3a1389e-2159-5c43-9038-463bf877d0d9

--- platform/ios-armv6/make/flags.mk
+++ platform/ios-armv6/make/flags.mk
@@ -1,1 +1,1 @@
-PLATFORM_COMPILE_FLAGS	:= -arch armv6 -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.3.sdk -gdwarf-2 -mthumb
+PLATFORM_COMPILE_FLAGS	:= -arch armv6 -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.3.sdk -gdwarf-2
