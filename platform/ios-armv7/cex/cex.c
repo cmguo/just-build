5e94489de0df594ce4b093fc92909a433b8c2cfe
zenzhang <zenzhang@pptv.com>
2013-12-02 09:30:32 +0000

    base ffmpeg编译支持 review:65846
    git-svn-id: http://svn01/repositories/r01/base@7764 b3a1389e-2159-5c43-9038-463bf877d0d9

--- platform/ios-armv7/cex/cex.c
+++ platform/ios-armv7/cex/cex.c
@@ -14,4 +14,24 @@ char ***_NSGetEnviron(void)
 {
     return (char ***)NULL;
 }
+
+iconv_t iconv_open(__const char *__tocode, __const char *__fromcode)
+{
+    return (iconv_t)(-1);
+}
+
+size_t iconv(
+    iconv_t __cd,
+    char **__restrict __inbuf,
+    size_t *__restrict __inbytesleft,
+    char **__restrict __outbuf,
+    size_t *__restrict __outbytesleft)
+{
+    return (size_t)(-1);
+}
+
+int iconv_close(iconv_t __cd)
+{
+    return -1;
+}
 __END_DECLS
