b8c086108ab83549d6f71d4923e5920c9c061d5b
isxxguo <isxxguo@pptv.com>
2014-05-17 14:24:23 +0800

    整理路径变量，去除重复header、include、lib目录
    Change-Id: I414c1ab5d6d419607a4993ff2883bca71d391fea

--- make/proj/depends.mk
+++ make/proj/depends.mk
@@ -10,19 +10,18 @@ include $(ROOT_MAKE_DIRECTORY)/func/depends.mk
 
 include $(PROJ_MAKE_DIRECTORY)/func/info.mk
 
-#iiiii := $(call fromat_depends,$(LOCAL_NAME),dirs_z,depends_z)
-
 define proj_print
 $(2) := $(strip $($(2)) $(1):$(call get_item,$(1),Type):$(call get_item,$(1),File):$(call get_item,$(1),Depends):$(call get_item,$(1),DependLibs)!)
 endef
 
-iiii := $(call tree_visit,$(LOCAL_NAME),Type File Depends DependLibs,proj_print,dirs_z,depends_z)
+DEPEND_ITEMS		:= $(call tree_visit,$(LOCAL_NAME),Type File Depends DependLibs,proj_print,DEPEND_ITEMS_TEMP,DEPEND_OUTPUT)
 
-DEPEND_FILES		:= $(shell echo "$(depends_z)" | awk -f $(ROOT_MAKE_DIRECTORY)/awk/depend.awk)
+DEPEND_FILES		:= $(shell echo "$(DEPEND_OUTPUT)" | awk -f $(ROOT_MAKE_DIRECTORY)/awk/depend.awk)
 
 SYSTEM_LIB 		:= $(filter-out /%,$(DEPEND_FILES))
 DEPEND_FILES 		:= $(filter /%,$(DEPEND_FILES))
 DEPEND_FILES  		:= $(addprefix $(PLATFORM_BUILD_DIRECTORY),$(DEPEND_FILES))
 
 DEPEND_PATHS		:= $(dir $(DEPEND_FILES))
+DEPEND_PATHS		:= $(call uniq,$(DEPEND_PATHS))
 DEPEND_NAMES		:= $(notdir$(DEPEND_FILES))
--- make/proj/target/paths.mk
+++ make/proj/target/paths.mk
@@ -7,7 +7,10 @@
 
 INC_PATHS		:= $(INC_PATHS) $(wildcard $(addsuffix include,$(DEPEND_PATHS)))
 INC_PATHS		:= $(INC_PATHS) $(HEADER_DIRECTORYS)
-INC_PATHS		:= $(INC_PATHS) $(PLATFORM_DIRECTORY)/include
+INC_PATHS		:= $(INC_PATHS) $(PLATFORM_INCLUDE_DIRECTORYS)
+INC_PATHS		:= $(INC_PATHS) $(addprefix $(ROOT_DIRECTORY)/,$(GLOBAL_INCLUDE_DIRECTORYS))
+
+INC_PATHS		:= $(call uniq,$(INC_PATHS))
 
 ifneq ($(CONFIG_LIB2),static) # dynamic static2, bin
 
@@ -16,7 +19,6 @@ LIB_PATHS		:= $(LIB_PATHS) $(DEPEND_PATHS)
 ifneq ($(CONFIG_LIB2),static2) # dynamic, bin
 
 LIB_PATHS		:= $(LIB_PATHS) $(PLATFORM_LIBRARY_DIRECTORYS)
-LIB_PATHS		:= $(LIB_PATHS) $(PLATFORM_DIRECTORY)/lib
 
 LIB_PATHS2		:= $(filter %/,$(DEPEND_FILES))
 LIB_PATHS2		:= $(call joinlist,:,$(strip $(LIB_PATHS2) $(LD_LIBRARY_PATH)))
@@ -24,3 +26,5 @@ LIB_PATHS2		:= $(call joinlist,:,$(strip $(LIB_PATHS2) $(LD_LIBRARY_PATH)))
 endif # dynamic, bin
 
 endif # dynamic static2, bin
+
+LIB_PATHS		:= $(call uniq,$(LIB_PATHS))
