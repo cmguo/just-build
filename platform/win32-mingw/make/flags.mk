61ce9633909dd28c72c333a750335410b20a29cc
isxxguo <isxxguo@pptv.com>
2016-01-11 10:10:47 +0800

    win32-mingw: 切换工具链到 i686-w64-mingw32，修正编译问题 _FILE_OFFSET_BITS -static-libgcc -static-libstdc++
    Change-Id: I3f8304b090ea8228327212d357d32627c0aa96a5

--- platform/win32-mingw/make/flags.mk
+++ platform/win32-mingw/make/flags.mk
@@ -2,6 +2,8 @@ PLATFORM_DISABLE_FLAGS		:= -fPIC -pthread -fvisibility
 
 PLATFORM_COMPILE_FLAGS		:= -fno-strict-aliasing -Wno-unused
 
+PLATFORM_LINK_FLAGS			:= -static-libgcc -static-libstdc++
+
 DYNAMIC_NAME_SUFFIX		:= .dll
 
 BIN_NAME_SUFFIX			:= .exe
